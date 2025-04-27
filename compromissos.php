12e9f3d266f7cabff5fb6bcbd58450673ae4bd29
Kauabasso <kauabasso8@gmail.com>
2025-04-26 21:57:17 -0300

    atualizado

--- compromissos.php
+++ compromissos.php
@@ -17,7 +17,7 @@ $comp = $dados->fetchAll(PDO::FETCH_ASSOC);
 
 foreach ($comp as &$compromisso) {
     $data = new DateTime($compromisso['data']);
-    $diaSemana = (int)$data->format('w'); // 0 = domingo, 6 = sábado
+    $diaSemana = (int)$data->format('w'); 
     $compromisso['fim_de_semana'] = ($diaSemana === 0 || $diaSemana === 6) ? 'Sim' : 'Não';
 }
 
