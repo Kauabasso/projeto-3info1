55f5d4c49f630a283b61ad7b7bbccdc96e2e79fb
Kauabasso <kauabasso8@gmail.com>
2025-04-26 21:54:38 -0300

    compromissos feito
    finalizado

--- compromissos.php
+++ compromissos.php
@@ -10,6 +10,12 @@ $dados = $pdo->query('SELECT * FROM compromissos');
 $comp = $dados->fetchAll(PDO::FETCH_ASSOC);
 
 
+foreach ($comp as &$compromisso) {
+    $data = new DateTime($compromisso['data']);
+    $diaSemana = (int)$data->format('w'); // 0 = domingo, 6 = sábado
+    $compromisso['fim_de_semana'] = ($diaSemana === 0 || $diaSemana === 6) ? 'Sim' : 'Não';
+}
+
 echo $twig->render('compromissos.html', [
     'titulo' => 'Compromissos',
     'compromissos' => $comp, //no html ela vai se chamar compras, aqui ela se chama comp
