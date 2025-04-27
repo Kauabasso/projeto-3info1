58c91b5a36b6ec94738fc1983ff1aa5004c5df37
Kauabasso <kauabasso8@gmail.com>
2025-04-26 22:10:13 -0300

    finalizado

--- compromissos.php
+++ compromissos.php
@@ -1,28 +1,21 @@
 <?php
-
 require_once('twig_carregar.php');
 require('inc/banco.php');
 require_once('session.php');
+require_once('vendor/autoload.php'); // se ainda não tiver
+use Carbon\Carbon;
 
-
-if (!isset($_SESSION['usuario'])) {
-    header("location: login3.php");
-    exit;
-}
-//Busca as compras no Banco
-$dados = $pdo->query('SELECT * FROM compromissos');
+$dados = $pdo->query('SELECT * FROM compromissos ORDER BY data ASC');
 
 $comp = $dados->fetchAll(PDO::FETCH_ASSOC);
 
-
 foreach ($comp as &$compromisso) {
-    $data = new DateTime($compromisso['data']);
-    $diaSemana = (int)$data->format('w'); 
-    $compromisso['fim_de_semana'] = ($diaSemana === 0 || $diaSemana === 6) ? 'Sim' : 'Não';
+    $data = Carbon::parse($compromisso['data']);
+    $compromisso['fim_de_semana'] = $data->isWeekend() ? 'Sim' : 'Não';
+ 
 }
 
 echo $twig->render('compromissos.html', [
     'titulo' => 'Compromissos',
-    'compromissos' => $comp, //no html ela vai se chamar compras, aqui ela se chama comp
-    'usuario' => $_SESSION['usuario']
+    'compromissos' => $comp,
 ]);
